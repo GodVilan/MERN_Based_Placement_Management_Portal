5290fd9d8447fe83404e4257512c99ced6a5234f
Srikanth Reddy Nandireddy <125195938+GodVilan@users.noreply.github.com>
2024-02-02 15:51:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(cors(
   }
 ));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://placement-management-portal.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const port = process.env.PORT || 5010;
 
