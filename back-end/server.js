dc3d1e0eeeba4ef8d5823e20bd3ffe5b0a191eb5
Srikanth Reddy Nandireddy <125195938+GodVilan@users.noreply.github.com>
2024-02-04 00:32:59 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -26,7 +26,7 @@ app.get("*", async => (req, res) => {
 const port = process.env.PORT || 5010;
 
 app.post('/', async (req, res) => {
-      req.setTimeout(500000);
+      req.setTimeout(100000);
     const { uid, password } = req.body;
 
     try {
