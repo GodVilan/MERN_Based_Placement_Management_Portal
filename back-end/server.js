37fd7a54eeed567b1a27a8915f6c701e0b64109c
Srikanth Reddy Nandireddy <125195938+GodVilan@users.noreply.github.com>
2024-02-03 23:05:12 +0530

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors(
   {
     origin : ["https://placement-portal-black.vercel.app"],
-    methods : ["POST", "GET"],
+    methods : ["POST", "GET", "PUT", "DELETE"],
     credentials: true
   }
 ));
